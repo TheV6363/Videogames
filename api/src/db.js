5b612e3e59f2fe6b3f5367b97a5950440aef780a
TheV6363 <videogamesvideogames63@gmail.com>
2024-02-24 16:04:50 -0300

    puedeser

--- api/src/db.js
+++ api/src/db.js
@@ -55,7 +55,7 @@ let sequelize =
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // }); // SUSTITUIR PARA HACER EL DEPLOY
-// const basename = path.basename(__filename);
+const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
